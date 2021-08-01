94949e0c9c4443b4d8d14d7b8f5db2dbd145a696
abarcalester <lester.howard.abarca@ucreativa.com>
2021-08-01 08:06:53 -0600

    Instalación de las dependencias de navigation y stack, además preparar la App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native'
 export default function App() {
   return (
     <View style={styles.container}>
@@ -10,12 +9,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
