e6bb09aa6e4a8c772d643ffade411bd4311f6b02
abarcalester <lester.howard.abarca@ucreativa.com>
2021-08-01 08:15:58 -0600

    La pantalla de screen ahora está retornando una vista

---
+++ screens/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+
+const Login = () => {
+    return (
+        <ScrollView>
+            <Text>
+                Esto es un texto de muestra, es algo increible.
+            </Text>
+        </ScrollView>
+    )
+}
+
+export default Login
