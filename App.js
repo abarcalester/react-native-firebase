37fb39abaccf0e419593f27f12d800108cd82dc1
abarcalester <lester.howard.abarca@ucreativa.com>
2021-08-01 08:10:38 -0600

    La navegación para el componente Login es configurada en App.js

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
+
+function MyStack () {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name={Login}
+        component={Login}
+      />
+    </Stack.Navigator>
+  )
+}
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack/>
+    </NavigationContainer>
   );
 }
