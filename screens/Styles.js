0a31fadbe0a87a505d6aca461628cde18ecc6bcb
abarcalester <lester.howard.abarca@ucreativa.com>
2021-08-01 08:38:53 -0600

    Adición del component para los estilos css y componente de Login completamente importado

---
+++ screens/Styles.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { StyleSheet } from 'react-native'
+
+const Styles = () => {
+    return (
+        StyleSheet.create({
+
+            textInput: {
+                outlineStyle: "none",
+                padding: 25,
+                borderBottomWidth: 1,
+                borderBottomColor: "#ccc" 
+            },
+            submitButton: {
+                margin: 10,
+                marginTop: 5,
+                marginBottom: 5
+            },
+            submitButtonTop: {
+                margin: 10,
+                marginTop: 20,
+                marginBottom: 5
+            },
+            alert: {
+                paddingTop: 10,
+                paddingLeft: 25,
+                color: '#D22B2B'
+            },
+            noalert: {
+                paddingTop: 10,
+                paddingLeft: 25
+            }
+        })
+    )
+}
+
+export default Styles()
