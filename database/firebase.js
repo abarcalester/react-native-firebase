f6fdfee9c6e7de6c7f3f17a72fcb7ed250b97bd6
abarcalester <lester.howard.abarca@ucreativa.com>
2021-08-01 08:38:04 -0600

    La carpeta y archivo para la configuración de firebase

---
+++ database/firebase.js
@@ -0,0 +1,12 @@
+
+
+let firebaseConfig = {
+    apiKey: "AIzaSyCAt9wRlYSymCcWyqHhAuJKG8GZkW3qMPs",
+    authDomain: "fir-complete-ae5a3.firebaseapp.com",
+    projectId: "fir-complete-ae5a3",
+    storageBucket: "fir-complete-ae5a3.appspot.com",
+    messagingSenderId: "876575875350",
+    appId: "1:876575875350:web:deaf59b18a32f96934f037"
+};
+
+firebase.initializeApp(firebaseConfig);
